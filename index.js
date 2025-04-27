ddf20da22bec5abc08810a4fba9166328186b961
Gobind Singh <gobindbhari@gmail.com>
2025-04-27 17:53:50 +0530

    init repo

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import express
+const express = require('express');
+
+// Create an express app
+const app = express();
+const PORT = 8000
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, this is a temporary Express server!');
+});
+app.get('/hello', (req, res) => {
+  res.send('Hello, this is a temporary Express server! \n from hello route');
+});
+
+// Set the server to listen on port 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
