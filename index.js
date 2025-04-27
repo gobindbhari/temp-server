82228d376fb6c56bafe719604dc025d262d9ff63
Gobind Singh <gobindbhari@gmail.com>
2025-04-28 03:12:45 +0530

    update port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Create an express app
 const app = express();
-const PORT = 8000
+const PORT = 8080
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
